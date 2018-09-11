6b926de811751233176a2a8be3773bf1c747af52
Collins Mugarura <collismugy@hotmail.com>
2018-09-11 21:23:51 +0100

    Initial commit

---
+++ src/app/employees/employee-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'app-employees',
+  templateUrl : './employee-list.component.html'
+})
+export class EmployeeListComponent {
+    private pageTitle = 'Employee List';
+}
