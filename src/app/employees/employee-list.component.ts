32b45819b17647834ce2f56a0f4d71348329e311
Collins Mugarura <collismugy@hotmail.com>
2018-09-14 16:42:47 +0100

    make employee service injectable

--- src/app/employees/employee-list.component.ts
+++ src/app/employees/employee-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Employee} from './models/employee,model';
 import {EmployeeService} from './providers/EmployeeService';
 
@@ -7,14 +7,33 @@ import {EmployeeService} from './providers/EmployeeService';
   selector : 'app-employees',
   templateUrl : './employee-list.component.html'
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
     employees: Employee[];
+    private employee : Employee= new class implements Employee {
+      firstName: 'Collins';
+      id: 1;
+      lastName: 'Mugarura';
+      salary: 42000;
+      startDate: '2017-11-09';
+    };
+    private emloyee1: Employee={
+      firstName : 'Sienna',
+      id: 2,
+      lastName : 'Mugarura',
+      salary :100,
+      startDate : '2018-03-09'
+    }
+    selectedEmployees: Employee[] = [this.employee, this.emloyee1];
 
     constructor(private employeeService: EmployeeService) {
 
     }
 
     showEmployees() {
-      this.employeeService.getEmployees().subscribe(data => this.employees = data);
+      this.employeeService.getEmployees().subscribe((employees: Employee[]) => this.employees = employees);
     }
+
+  ngOnInit(): void {
+      this.showEmployees();
+  }
 }
